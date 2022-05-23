1876d0162161d2d4613902a7f65db6704df35464
rezwanrahimr <rezwanrahim69@gmail.com>
2022-05-23 19:29:17 +0600

    update tools url

--- src/pages/Home/Parts.js
+++ src/pages/Home/Parts.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import AllParts from './AllParts';
 
 const Parts = () => {
     const [parts,setParts] = useState([]);
     useEffect(()=>{
-        fetch('http://localhost:5000/tools')
+        fetch('https://enigmatic-ridge-01425.herokuapp.com/tools')
         .then(res => res.json())
         .then(data => setParts(data))
     },[])
+    console.log(parts);
     return (
         <div className='grid grid-cols-3 gap-4 my-5'>
 
             {
-               parts.slice(0,6).map((Tools) => <AllParts key={Tools._id} Tools={Tools}  ></AllParts>)
+               parts.slice(0,6).map(Tools => <AllParts key={Tools._id} Tools={Tools}  ></AllParts>)
             }
             
         </div>
